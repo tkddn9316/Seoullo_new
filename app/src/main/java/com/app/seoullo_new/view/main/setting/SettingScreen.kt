d54281662b62af185cd1387626ce87fd8642f72b
tkddn9316 <tkddn9316@etomato.com>
2025-03-05 17:41:36 +0900

    목록 숨기기 UI 작업

--- app/src/main/java/com/app/seoullo_new/view/main/setting/SettingScreen.kt
+++ app/src/main/java/com/app/seoullo_new/view/main/setting/SettingScreen.kt
@@ -6,15 +6,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.text.TextAutoSize
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -23,7 +32,9 @@ import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.app.seoullo_new.R
@@ -95,6 +106,13 @@ fun SettingScreen(
             )
             ThemeSetting { viewModel.openThemeDialog() }
             LanguageSetting { viewModel.openLanguageDialog() }
+            HideTodayWatchedListSetting(
+                title = stringResource(R.string.hide_list_title),
+                description = stringResource(R.string.hide_list_description),
+                onItemClick = {
+
+                }
+            )
             HorizontalDivider(thickness = 1.dp, color = MaterialTheme.colorScheme.outlineVariant)
 
             // About
@@ -246,6 +264,63 @@ fun LogoutSetting(
                 contentDescription = null
             )
         }
+    )
+}
+
+@Composable
+fun HideTodayWatchedListSetting(
+    modifier: Modifier = Modifier,
+    title: String,
+    description: String,
+    onItemClick: () -> Unit
+) {
+    val colors = ListItemDefaults.colors()
+    val descriptionColor = MaterialTheme.colorScheme.onBackground
+    var checked by rememberSaveable { mutableStateOf(false) }
+
+    ListItem(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        headlineContent = {
+            Text(
+                text = title,
+                overflow = TextOverflow.Ellipsis,
+                color = descriptionColor
+            )
+        },
+        supportingContent = {
+            BasicText(
+                text = description,
+                maxLines = 1,
+                overflow = TextOverflow.Visible,
+                autoSize = TextAutoSize.StepBased(
+                    minFontSize = 8.sp,
+                    maxFontSize = 14.sp,
+                    stepSize = 2.sp
+                ),
+                color = { descriptionColor }
+            )
+        },
+        leadingContent = {
+            Icon(
+                ImageVector.vectorResource(id = R.drawable.ic_setting_hide_list),
+                contentDescription = null
+            )
+        },
+        trailingContent = {
+            Switch(
+                checked = checked,
+                onCheckedChange = {
+                    checked = it
+                }
+            )
+        },
+        colors = ListItemDefaults.colors(
+            headlineColor = colors.headlineColor,
+            supportingColor = colors.supportingTextColor,
+            trailingIconColor = colors.trailingIconColor
+        )
     )
 }
 
